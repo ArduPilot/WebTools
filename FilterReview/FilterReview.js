73cab998aeb854775ce307e4e3107d6632fdd42d
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2024-09-07 16:02:28 +0100

    FilterReview: RPM tracking: add support for new RPM message

--- FilterReview/FilterReview.js
+++ FilterReview/FilterReview.js
@@ -139,7 +139,28 @@ class ThrottleTarget extends NotchTarget {
 
 class RPMTarget extends NotchTarget {
     constructor(log, instance, mode_value) {
-        super(log, "RPM", "rpm" + instance, "RPM" + instance, mode_value)
+        super(null, null, null, "RPM" + instance, mode_value)
+
+        const msg_name = "RPM"
+        if (!(msg_name in log.messageTypes)) {
+            // Log message not found
+            return
+        }
+
+        if ("instances" in log.messageTypes.RPM) {
+            // New instance RPM message
+            const inst = instance - 1
+            if (inst in Object.keys(log.messageTypes.RPM.instances)) {
+                this.data.time = TimeUS_to_seconds(log.get_instance(msg_name, inst, "TimeUS"))
+                this.data.value = log.get_instance(msg_name, inst, "RPM")
+            }
+
+        } else {
+            // Old log message containing both rpm 1 and 2
+            this.data.time = TimeUS_to_seconds(log.get(msg_name, "TimeUS"))
+            this.data.value = log.get(msg_name, "rpm" + instance)
+
+        }
     }
 
     get_target(config, rpm) {
