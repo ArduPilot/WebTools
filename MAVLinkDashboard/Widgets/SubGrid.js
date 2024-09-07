be490f899730f35626f554f7b5ad8d3d9facacd8
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2024-09-07 22:56:35 +0100

    MAVLinkDashboard: Widgets: SubGrid: send MAVLink messages onto sub widgets

--- MAVLinkDashboard/Widgets/SubGrid.js
+++ MAVLinkDashboard/Widgets/SubGrid.js
@@ -262,6 +262,16 @@ class WidgetSubGrid extends WidgetBase {
         }
     }
 
+    MAVLink_msg_handler(msg) {
+        if (this.grid == null) {
+            return
+        }
+
+        // Forward message on to sub widgets
+        for (const widget of this.grid.getGridItems()) {
+            widget.MAVLink_msg_handler(msg)
+        }
+    }
 
     load_grid() {
 
