03bd9bd59efdd28239ef09d805253ac3526fa9fa
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2024-09-24 02:37:37 +0100

    FilterReview: use new fft scale helpers

--- FilterReview/FilterReview.js
+++ FilterReview/FilterReview.js
@@ -924,64 +924,20 @@ function calculate_transfer_function() {
     document.getElementById("calculate_filters").disabled = true
 }
 
-
 // Get configured amplitude scale
 function get_amplitude_scale() {
-
     const use_DB = document.getElementById("ScaleLog").checked
     const use_PSD = document.getElementById("ScalePSD").checked
 
-    var ret = {}
-    if (use_PSD) {
-        ret.fun = function (x) { return array_mul(x,x) } // x.^2
-        ret.scale = function (x) { return array_scale(array_log10(x), 10.0) } // 10 * log10(x)
-        ret.label = "PSD (dB/Hz)"
-        ret.hover = function (axis) { return "%{" + axis + ":.2f} dB/Hz" }
-        ret.window_correction = function(correction, resolution) { return ((correction.energy**2) * 0.5) / resolution }
-        ret.quantization_correction = function(window_correction) { return 1 / Math.sqrt(window_correction) }
-
-    } else if (use_DB) {
-        ret.fun = function (x) { return x }
-        ret.scale = function (x) { return array_scale(array_log10(x), 20.0) } // 20 * log10(x)
-        ret.label = "Amplitude (dB)"
-        ret.hover = function (axis) { return "%{" + axis + ":.2f} dB" }
-        ret.correction_scale = 1.0
-        ret.window_correction = function(correction, resolution) { return correction.linear }
-        ret.quantization_correction = function(window_correction) { return 1 / window_correction }
-
-    } else {
-        ret.fun = function (x) { return x }
-        ret.scale = function (x) { return x }
-        ret.label = "Amplitude"
-        ret.hover = function (axis) { return "%{" + axis + ":.2f}" }
-        ret.window_correction = function(correction, resolution) { return correction.linear }
-        ret.quantization_correction = function(window_correction) { return 1 / window_correction }
-
-    }
-
-    return ret
+    return fft_amplitude_scale(use_DB, use_PSD)
 }
 
 // Get configured frequency scale object
 function get_frequency_scale() {
-
     const use_RPM = document.getElementById("freq_Scale_RPM").checked
+    const log_scale = document.getElementById("freq_ScaleLog").checked
 
-    var ret = {}
-    if (use_RPM) {
-        ret.fun = function (x) { return array_scale(x, 60.0) }
-        ret.label = "RPM"
-        ret.hover = function (axis) { return "%{" + axis + ":.2f} RPM" }
-
-    } else {
-        ret.fun = function (x) { return x }
-        ret.label = "Frequency (Hz)"
-        ret.hover = function (axis) { return "%{" + axis + ":.2f} Hz" }
-    }
-
-    ret.type = document.getElementById("freq_ScaleLog").checked ? "log" : "linear"
-
-    return ret
+    return fft_frequency_scale(use_RPM, log_scale)
 }
 
 function get_alias_obj(FFT) {
