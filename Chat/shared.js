3a84de69725c238c4fd7ea5e8ef3f5cecf5ba21b
esatiyev <esetiyev@std.beu.edu.az>
2025-06-30 06:33:45 +0900

    Chat: Refactor chat: update layout, add shared.js and style.css

---
+++ Chat/shared.js
@@ -0,0 +1,11 @@
+// shared.js
+export let MAVLink = null;
+export let mavlink_ws = null;
+
+export function setMAVLink(instance) {
+    MAVLink = instance;
+}
+
+export function setMavlinkWS(ws) {
+    mavlink_ws = ws;
+}
